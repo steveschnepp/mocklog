890a1d0ed166d589c0d630fa5dbc5bb976c1d7d1
Steve Schnepp <steve.schnepp@pwkf.org>
2021-03-26 22:06:16 +0100

    init

---
+++ src/main/java/org/pwkf/httplog/HttplogApplication.java
@@ -0,0 +1,13 @@
+package org.pwkf.httplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttplogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttplogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pwkf/httplog/HttplogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pwkf.httplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttplogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
