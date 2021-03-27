98736a941eb8983df0599e7192bcb44097f7fe38
Steve Schnepp <steve.schnepp@pwkf.org>
2021-03-27 11:01:53 +0100

    not using JSP

--- src/main/java/org/pwkf/httplog/controller/MainController.java
+++ src/main/java/org/pwkf/httplog/controller/MainController.java
@@ -3,6 +3,7 @@ package org.pwkf.httplog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.java.Log;
 
@@ -12,8 +13,9 @@ import lombok.extern.java.Log;
 public class MainController {
 
     @GetMapping
+    @ResponseBody
 	String root() {
 	log.info("ok");
 	return "ok";
-	}
+    }
 }
